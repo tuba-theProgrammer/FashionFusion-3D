d68fa86bfbec8fd6e4399d9d1b07782faaa4d795
unknown <tubarajput92@gmail.com>
2024-09-13 23:50:14 +0500

    shirt redesign

--- src/_components/Tabs/index.tsx
+++ src/_components/Tabs/index.tsx
@@ -14,25 +14,12 @@ interface TabProps {
 }
 
 const Tab: React.FC<TabProps> = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
-  const snap = useSnapshot(state);
-
-  // Define styles for active and inactive tabs
-  const activeStyles = isFilterTab && isActiveTab
-    ? { backgroundColor: snap.color, opacity: 0.5 }
-    : { backgroundColor: 'transparent', opacity: 1 };
+ 
 
   return (
     <div
-      key={tab.name}
-      className={`tab-btn ${isFilterTab ? 'rounded-full glassmorphism' : 'rounded-4'}`}
-      onClick={handleClick}
-      style={activeStyles}
     >
-      <img
-        src={tab.icon}
-        alt={tab.name}
-        className={`${isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 object-contain'}`}
-      />
+       tab
     </div>
   );
 };
